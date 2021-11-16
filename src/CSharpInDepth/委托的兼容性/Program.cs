68b92763f42408bd0a63e9935c2f25f18513b206
MingsonZheng <MingsonZheng@outlook.com>
2021-11-16 18:14:41 +0800

    委托的兼容性

---
+++ src/CSharpInDepth/委托的兼容性/Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintAnything(object obj)
+{
+    Console.WriteLine(obj);
+}
+
+string ProviderAnything()
+{
+    return string.Empty;
+}
+
+Printer p1 = new Printer(PrintAnything);
+Printer p2 = PrintAnything;
+
+GeneralPrinter generalPrinter = PrintAnything;
+Printer p3 = new Printer(generalPrinter);// 构建一个Printer来封装GeneralPrinter
+//Printer p4 = generalPrinter;
+
+StringProvider stringProvider = new StringProvider(ProviderAnything);
+ObjectProvider objectProvider = new ObjectProvider(stringProvider);
+
+public delegate void Printer(string message);
+public delegate void GeneralPrinter(object obj);
+public delegate string StringProvider();
+public delegate object ObjectProvider();
