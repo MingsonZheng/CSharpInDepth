867026aa90ef0afe58087a59f8d3e209f04e9b42
MingsonZheng <MingsonZheng@outlook.com>
2021-11-17 10:02:41 +0800

    委托的兼容性

--- src/委托的兼容性/Program.cs
+++ src/委托的兼容性/Program.cs
@@ -8,17 +8,28 @@ string ProviderAnything()
     return string.Empty;
 }
 
-Printer p1 = new Printer(PrintAnything);
+void PrintLong(long l)
+{
+    Console.WriteLine(l);
+}
+
+Printer p1 = new Printer(PrintAnything);// string类型的引用可以通过一致性转换变为object类型的引用
 Printer p2 = PrintAnything;
 
-GeneralPrinter generalPrinter = PrintAnything;
-Printer p3 = new Printer(generalPrinter);// 构建一个Printer来封装GeneralPrinter
-//Printer p4 = generalPrinter;
+GeneralPrinter g1 = PrintAnything;  
+Printer p3 = new Printer(g1);// 使用委托来创建另一个委托
 
 StringProvider stringProvider = new StringProvider(ProviderAnything);
-ObjectProvider objectProvider = new ObjectProvider(stringProvider);
+ObjectProvider objectProvider = new ObjectProvider(stringProvider);// 返回类型同理
+
+Int64Printer int64Printer = new Int64Printer(PrintLong);
+//Int32Printer int32Printer = new Int32Printer(int64Printer);// 参数或返回值之间兼容性必须满足一致性转换规则
 
 public delegate void Printer(string message);
 public delegate void GeneralPrinter(object obj);
+
 public delegate string StringProvider();
 public delegate object ObjectProvider();
+
+public delegate void Int32Printer(int x);
+public delegate void Int64Printer(long x);
